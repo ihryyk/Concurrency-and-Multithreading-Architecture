35de5051b86c69b951b4c0b171010979cb068434
Ihor Berezovskyi <ihor_berezovskyi@epam.com>
2024-04-25 14:58:16 +0300

    Task2 - Finish Task 2 (Deadlocks)

---
+++ task2/SumNumberThread.java
@@ -0,0 +1,26 @@
+package task2;
+
+import java.util.List;
+
+public class SumNumberThread implements Runnable {
+
+    private final List<Double> numbers;
+
+    public SumNumberThread(List<Double> numbers) {
+        this.numbers = numbers;
+    }
+
+    @Override
+    public void run() {
+        do {
+            synchronized (numbers) {
+                double sum = 0;
+                for (Double number : numbers) {
+                    sum += number;
+                }
+                System.out.println("Sum: " + sum);
+            }
+        }while (numbers.size()<1000);
+    }
+
+}
